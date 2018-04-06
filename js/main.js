b2b95ca22844c1c34d2d064811ac97233468e801
cptfinch <cptfinch@gmail.com>
2018-04-06 22:06:16 +0100

    toggling between F and C not working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
 function getWeather() {
-//  var requestURL = "https://cors.io/?https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key";
-var requestURL= "http://api.openweathermap.org/data/2.5/forecast?id=2648772&APPID=fc55b6a55018f9461b19089fb9a3dc68";
+  var requestUrl= "http://api.openweathermap.org/data/2.5/forecast?id=2648772&appid=fc55b6a55018f9461b19089fb9a3dc68";
   var request = new XMLHttpRequest();
-  request.open('GET', requestURL);
+  request.open('get', requestUrl);
   request.responseType = 'json';
   request.send();
   request.onload = function () {
     var weather = request.response;
-    showWeather(weather['list'][0]['weather'][0]['description']);
+    showWeatherDesc(weather['list'][0]['weather'][0]['description']);
+    showWeatherTemp(weather['list'][0]['main']['temp']);
   }
 }
-function showWeather(weather) {
+function showWeatherDesc(weather) {
   const weatherDiv = document.getElementById('weather-div');
   weatherDiv.innerHTML = weather;
 }
+function showWeatherTemp(tempInKelvin) { 
+  const weatherTempDiv= document.getElementById('weather-temp-div');
+  if (inCelciusState){
+    temperature = tempInKelvin-273;
+  } else {
+    temperature = (9.0/5.0) *(tempInKelvin - 273) + 32;
+  }
+  temperature=temperature.toFixed(1);
+  var unit = 'C';
+  if (inCelciusState) {
+    unit='C';
+  } else {
+    unit = 'F'
+  }
+  weatherTempDiv.innerHTML = `${temperature} ${unit}`;
+}
+
 document.querySelector('#get-weather-button').addEventListener('click',getWeather);
+
+//deal with toggling between c and f in the display of the temperature. 
+inCelciusState=true;
+var temperature = 0;
+
+function showChangeInUnits() {
+  var newTemperature = 0;
+  if (inCelciusState) {
+    newTemperature = (9.0/5.0)*temperature + 32; 
+    newTemperature = newTemperature.toFixed(1);
+    newTemperature = `${newTemperature}F`;
+    weatherTempDiv.innerHTML = newTemperature;
+  } else {
+      newTemperature = (5.0/9.0)*temperature - 32; 
+      newTemperature = newTemperature.toFixed(1);
+      newTemperature = `${newTemperature}C`;
+      weatherTempDiv.innerHTML = newTemperature;
+  }
+}
+
+function toggleBetweenCandF() {
+  inCelciusState = !inCelciusState;
+  showChangeInUnits();
+}
+document.querySelector('#weather-temp-div').addEventListener('click',toggleBetweenCandF());
